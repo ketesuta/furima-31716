63fa0b0c4ca4d72a619230c0468ecd06c6152e76
森田一 <moritahajime@moritahajimenoMacBook-Air.local>
2020-11-23 19:06:12 +0900

    バリデーションの記述
    バリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :birthday, presence: true
+  validates :email, uniqueness: true
+  validates :password, length: { minimum: 6 }
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+    validates :first_name
+    validates :last_name
+
+  with_options presence: true, format: { with:  /^[ァ-ンー－]+$/ }
+    validates :first_name_kana
+    validates :last_name_kana
 end
