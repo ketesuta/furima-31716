d056586b697951975d2b0ae22913a3fadec080da
森田一 <moritahajime@moritahajimenoMacBook-Air.local>
2020-11-27 12:26:58 +0900

    カテゴリーのid追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belomgs_to :category
+  validates :imege, :item_name, :item_explanation, presence: true
+
+  validates :category_id, numericality: { other_than: 1 }
 end
