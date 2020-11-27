836eba51ab4a1d51ffb4fbf4ef9854275684f1e3
森田一 <moritahajime@moritahajimenoMacBook-Air.local>
2020-11-27 12:39:37 +0900

    item_statusモデルの追加
    バリデーションの記述
    idの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,12 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belomgs_to :category
+  belomgs_to :item_status
+
+
   validates :imege, :item_name, :item_explanation, presence: true
 
   validates :category_id, numericality: { other_than: 1 }
+  validates :item_status_id, numericality: { other_than: 1 }
+
 end
