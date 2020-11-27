9a0cf733557d13306556616ff319cb2adead14bb
森田一 <moritahajime@moritahajimenoMacBook-Air.local>
2020-11-27 15:24:41 +0900

    prefecture（発注元の地域、都道府県）モデルの追加
    prefectureのバリデーション、idの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,8 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belomgs_to :category
   belomgs_to :item_status
+  belomgs_to :shipping_charge
+  belomgs_to :prefecture
 
 
   validates :imege, :item_name, :item_explanation, presence: true
@@ -10,5 +12,6 @@ class Item < ApplicationRecord
     validates :category_id
     validates :item_status_id
     validates :shipping_charge_id
+    validates :prefecture_id
   end
 end
