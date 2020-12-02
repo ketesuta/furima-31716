c2ba218fc144d52d70384c5af565558bf1e294e4
森田一 <moritahajime@moritahajimenoMacBook-Air.local>
2020-12-02 16:02:58 +0900

    コントローラーに記述
    editパスの記述
    move_to_editメソッドで出品者以外のアクセス制限
    コメントアウトの削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,21 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_items, only: [:edit, :update, :show]
+  before_action :move_to_edit, only: [:edit]
+
   def index
     @items = Item.order("created_at DESC")
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render edit
+    end
   end
 
   def edit
@@ -28,4 +38,15 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:item_name, :item_explanation, :category_id, :item_status_id, :shipping_charge_id, :prefecture_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
   end
+
+  def set_items
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_edit
+    unless current_user == @item.user
+      redirect_to root_path
+    end
+  end
+
 end
