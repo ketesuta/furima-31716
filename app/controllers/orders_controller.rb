34fed944fbb23a67208aa21d44bd922f71fe538f
森田一 <moritahajime@moritahajimenoMacBook-Air.local>
2020-12-03 19:32:04 +0900

    コントローラにparamsの許可
    あとバリデーションの記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,4 +8,14 @@ class OrdersController < ApplicationController
 
   def create
   end
+
+  private
+
+  def address_params
+    params.permit(:postal_code, :prefecture_id,:municipality,:address,:building_name,:phone_number)
+  end
+
+  def buy_item_params
+    params.merge(user_id: user.id,item_id :item.id)
+  end
 end
--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -2,5 +2,12 @@ class ItemOder
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture, :municipality, :address,:building_name,:phone_number
 
-  validates :postal_code
+  validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  validates :prefecture_id, resence: true, numericality: { other_than: 1 }
+  validates :municipality, :address, presence: true
+  validates :phone_number,\d{10,11}
+
+  def save
+
+  end
 end
